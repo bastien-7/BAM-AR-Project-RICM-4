749d1bfd6a9b30c2a59fef17415e174b7ed212d9
HugoGDA <gdahugo@gmail.com>
2018-03-16 10:24:03 +0100

    BAMAgentClassLoaderClass

---
+++ src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
@@ -0,0 +1,19 @@
+package jus.aor.mobilagent.kernel;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class BAMServerClassLoader extends URLClassLoader{
+
+	public BAMServerClassLoader(URL[] urls) {
+		super(urls);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addURL(URL url) {
+		// TODO Auto-generated method stub
+		super.addURL(url);
+	}
+
+}
