0d1674f3e5d25ebdd57aa3a463fb1974054a3187
TERRIER <bastien.terrier7@gmail.com>
2018-03-23 11:40:04 +0100

    adding agent unimplemented methods

--- src/jus/aor/mobilagent/kernel/Agent.java
+++ src/jus/aor/mobilagent/kernel/Agent.java
@@ -1,5 +1,29 @@
 package jus.aor.mobilagent.kernel;
 
-public class Agent {
+public class Agent implements _Agent{
+
+	@Override
+	public void run() {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void init(AgentServer agentServer, String serverName) {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void reInit(AgentServer server, String serverName) {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void addEtape(Etape etape) {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
 
 }
