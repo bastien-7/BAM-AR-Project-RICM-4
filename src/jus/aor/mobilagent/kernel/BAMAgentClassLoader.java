9744f9bd87e286f01fbfd5dcaa5c7403ec184d82
TERRIER <bastien.terrier7@gmail.com>
2018-03-23 11:05:06 +0100

    BAMAgentClassLoader

--- src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
+++ src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
@@ -1,10 +1,18 @@
 package jus.aor.mobilagent.kernel;
 
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.jar.JarException;
+
 public class BAMAgentClassLoader extends ClassLoader{
 	
-	public BAMAgentClassLoader(String jarName, ClassLoader parent) {
+	public BAMAgentClassLoader(String jarName, ClassLoader parent) throws JarException, IOException {
 		super(parent);
-		
-		
+		Jar my_jar = new Jar(jarName);
+		while (my_jar.iterator().hasNext()) {
+			Entry<String,byte[]> ent = my_jar.iterator().next();
+			defineClass(ent.getKey(),ent.getValue(),0,ent.getValue().length);
+			System.out.println("BAMAgentClassLoader : charge class "+ent.getKey());
+		}
 	}
 }
