b7e0959b55b7e357ecf782377b9a0b0bed492f23
TERRIER <bastien.terrier7@gmail.com>
2018-04-05 12:30:01 +0200

    BAMAgentClassLoader

--- src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
+++ src/jus/aor/mobilagent/kernel/BAMAgentClassLoader.java
@@ -13,16 +13,14 @@ import java.util.jar.JarOutputStream;
 public class BAMAgentClassLoader extends ClassLoader {
 	// we can't add extra Maps to an existing Jar, so let's store a MAP instead of
 	// storing a JAR
-	private Map<String, byte[]> MapJAR;
+	private Jar my_jar;
 
 	BAMAgentClassLoader(String jarName, ClassLoader parent) throws JarException, IOException {
 		super(parent);
-		this.MapJAR = new HashMap<String, byte[]>();
-		Jar my_jar = new Jar(jarName);
+		this.my_jar = new Jar(jarName);
 		while (my_jar.iterator().hasNext()) {
 			Entry<String, byte[]> ent = my_jar.iterator().next();
 			// Save JAR information in the MAP for future export
-			MapJAR.put(ent.getKey(), ent.getValue());
 			defineClass(ent.getKey(), ent.getValue(), 0, ent.getValue().length);
 			System.out.println("BAMAgentClassLoader : charge class " + ent.getKey());
 		}
@@ -31,10 +29,10 @@ public class BAMAgentClassLoader extends ClassLoader {
 
 	BAMAgentClassLoader(ClassLoader parent) {
 		super(parent);
-		this.MapJAR = new HashMap<String, byte[]>();
 	}
 
 	void integrateCode(Jar my_jar) {
+		this.my_jar=my_jar;
 		while (my_jar.iterator().hasNext()) {
 			Entry<String, byte[]> ent = my_jar.iterator().next();
 			defineClass(ent.getKey(), ent.getValue(), 0, ent.getValue().length);
@@ -47,25 +45,14 @@ public class BAMAgentClassLoader extends ClassLoader {
 	}
 
 	Jar extractCode() throws IOException {
-		// Creation of a new file
-		// Copy the current class loader (that we saved in MapJAR) in the file
-		// return a new Jar (filename)
 
-		File my_file = File.createTempFile("my_file_name", ".txt");
-		JarOutputStream JOS = new JarOutputStream(new FileOutputStream(my_file));
-		for (String s : this.MapJAR.keySet()) {
-			JOS.putNextEntry(new JarEntry(s));
-			JOS.write(this.MapJAR.get(s));
-		}
-		JOS.flush();
-		JOS.close();
 
-		return new Jar(my_file.getPath());
+		return this.my_jar;
 
 	}
 
 	public String toString() {
-		return "BAMAgentClassLoader : parent :" + this.getParent().toString() + "MAP :" + MapJAR.toString();
+		return "BAMAgentClassLoader : parent :" + this.getParent().toString() + "Jar :" +this.my_jar.toString();
 
 	}
 }
